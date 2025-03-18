d6d9c1b3270e4ced1a7455eabb9646354e34d6b0
RedIODev <redio.development@gmail.com>
2025-03-18 04:21:33 +0100

    added Industrialcraft dependency and macerator test recipe

--- src/main/java/dev/redio/mods/industrialadvancement/Items.java
+++ src/main/java/dev/redio/mods/industrialadvancement/Items.java
@@ -7,10 +7,13 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import dev.redio.mods.industrialadvancement.blocks.BlockCrudOil;
 import dev.redio.mods.industrialadvancement.blocks.BlockQuartzSand;
 import dev.redio.mods.industrialadvancement.items.ItemCrudOilBucket;
+import dev.redio.mods.industrialadvancement.items.ItemSilicon;
+import ic2.api.recipe.RecipeInputItemStack;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fluids.FluidContainerRegistry;
 import net.minecraftforge.fluids.FluidRegistry;
@@ -18,6 +21,7 @@ import net.minecraftforge.fluids.FluidRegistry;
 public class Items {
     public static final List<Item> items = new ArrayList<Item>();
     public static ItemCrudOilBucket crud_oil_bucket;
+    public static ItemSilicon silicon;
 
     public static void registerItems() {
         items.add(crud_oil_bucket = new ItemCrudOilBucket(Blocks.crud_oil_block));
@@ -26,6 +30,9 @@ public class Items {
         FluidContainerRegistry.registerFluidContainer(Blocks.crud_oil, new ItemStack(crud_oil_bucket),
                 new ItemStack(net.minecraft.init.Items.bucket));
 
+        items.add(silicon = new ItemSilicon());
+        ic2.api.recipe.Recipes.macerator.addRecipe(
+            new RecipeInputItemStack(new ItemStack(Item.getItemFromBlock(Blocks.quartz_sand))), null, new ItemStack(silicon));
         for (Item item : items) {
             GameRegistry.registerItem(item, item.getUnlocalizedName());
         }
---
+++ src/main/java/dev/redio/mods/industrialadvancement/items/ItemSilicon.java
@@ -0,0 +1,13 @@
+package dev.redio.mods.industrialadvancement.items;
+
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ItemSilicon extends Item {
+    
+    public ItemSilicon() {
+        setCreativeTab(CreativeTabs.tabMisc);
+        setUnlocalizedName("silicon");
+    }
+}
